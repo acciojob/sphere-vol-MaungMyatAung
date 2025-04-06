4580938990ba14321d51f559ee6c95e5cefd181c
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-06 14:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function volume_sphere() {
     //Write your code here
+	let radius=document.getElementById("redius").value;
+
+	radius = parseFloat(radius);
+
+	if (isNaN(radius)|| radius<0) {
+
+		document.getElementById("volume").value="NaN";
+		return;
+		
+	}
+	const volume=(4/3)*Math.PI * Math.pow(radius,3);
+
+	document.getElementById("volume").value=value.toFixed(4);
   
 } 
 
